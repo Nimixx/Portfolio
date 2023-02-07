720ff5113bf2e2398d076cae7f4530c4174af0ad
Tadeas Thelen <ttadeasthelen@gmail.com>
2023-02-08 00:35:01 +0100

    Edit bg

--- src/components/Homepage/Title.tsx
+++ src/components/Homepage/Title.tsx
@@ -12,33 +12,34 @@ export default function Title() {
   };
 
   return (
-    <div className="title__section--wrapper mt-52">
+    <div className='title__section--wrapper mt-52'>
       <motion.h1
         initial={{ opacity: 0, translateX: -60 }}
         whileInView={{ opacity: 1, translateX: 0 }}
         transition={{ duration: 0.3 }}
-        className="primary__heading purple-pink"
+        className='primary__heading purple-pink'
       >
-        Nimixx <span className="secondary__heading orange-yellow">Tadeáš</span>
+        Nimixx#7777
+        {/* <span className='secondary__heading orange-yellow'>Tadeáš</span> */}
       </motion.h1>
       <motion.h2
         initial={{ opacity: 0, translateX: 60 }}
         whileInView={{ opacity: 1, translateX: 0 }}
         transition={{ duration: 0.3 }}
-        className="text-yellow-400 secondary__heading"
+        className='text-yellow-400 secondary__heading'
       >
-        <span className="text-white">Junior</span> Frontend{' '}
-        <span className="text-white">Developer</span>
+        <span className='text-white'>Junior</span> Frontend{' '}
+        <span className='text-white'>Developer</span>
       </motion.h2>
       <motion.div
         initial={{ opacity: 0, translateY: 60 }}
         whileInView={{ opacity: 1, translateY: 0 }}
         transition={{ duration: 0.3 }}
-        className="title__wrapper hidden md:flex"
+        className='title__wrapper hidden md:flex'
       >
-        <h3 className="text-sky-400 tertiary__heading">
-          Baví mě <span className="text-green-400">moderní</span>,{' '}
-          <span className="underline underline-offset-4 decoration-sky-200">
+        <h3 className='text-sky-400 tertiary__heading'>
+          Baví mě <span className='text-green-400'>moderní</span>,{' '}
+          <span className='underline underline-offset-4 decoration-sky-200'>
             rychlé aplikace{' '}
           </span>
           a rád pracuju v Reactu
@@ -50,19 +51,19 @@ export default function Title() {
             repeatType: 'loop',
             duration: 10,
           }}
-          className="w-10"
+          className='w-10'
           src={ReactImg}
-          alt=""
+          alt=''
         />
       </motion.div>
       <motion.div
         initial={{ opacity: 0, translateY: 60 }}
         whileInView={{ opacity: 1, translateY: 0 }}
         transition={{ duration: 0.3 }}
-        className="title__wrapper"
+        className='title__wrapper'
       >
-        <i className="mt-5 text-4xl animate-bounce">🤜</i>
-        <button onClick={handleClick} className="story__btn">
+        <i className='mt-5 text-4xl animate-bounce'>🤜</i>
+        <button onClick={handleClick} className='story__btn'>
           Můj příběh
         </button>
       </motion.div>
